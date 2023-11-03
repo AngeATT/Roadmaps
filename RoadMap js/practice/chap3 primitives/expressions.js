3445eda853f7a98626be52396dc06e5580303b68
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-11-03 10:12:54 +0000

    gg

--- RoadMap js/practice/chap3 primitives/expressions.js
+++ RoadMap js/practice/chap3 primitives/expressions.js
@@ -1,8 +1,13 @@
 let a = "10";
-console.log(a.jksjk)
+console.log(a.jksjk) //undefined "jksjk" n'existe pas pour un string
 //console.log(b.iufiu) //referenceError b non declarer
 let c;
 //console.log(c.gg) //typeError c undefined ou null
 
 let d = "éé";
-console.log(d())//TypeError d 
+//console.log(d())//TypeError d 
+
+let ab = [1,2,3]
+console.log(ab["2"])
+
+console.log(1 + "2")
